5c5cfad0826c0cc9b1ede3931892b2a70299d3cd
langhuai <757501172@qq.com>
2016-11-23 00:19:54 +0800

    Update urlredirect.js

--- Public/pc/style/urlredirect.js
+++ Public/pc/style/urlredirect.js
@@ -4,7 +4,7 @@ function urlredirect() {
 	if ((sUserAgent.match(/(ipod|iphone os|midp|ucweb|android|windows ce|windows mobile)/i))) {
 		// 只适用盘古建站，PC跳转移动端
 		var thisUrl = window.location.href;
-		window.location.href = thisUrl.substr(0,thisUrl.lastIndexOf('/')+1)+'wap/';
+		window.location.href = thisUrl.substr(0,thisUrl.lastIndexOf('/')+1)+'Mobile/';
 		
 	}
 }
